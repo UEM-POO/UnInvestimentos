d77af7fc88a4fb3eae595492e250c836437ad32c
Jose Seie <joseeugenioseie@hotmail.com>
2017-08-01 10:40:14 +0200

    Controller criado

---
+++ src/uem/aulas/semana1/FuncionarioController.java
@@ -0,0 +1,24 @@
+
+package uem.aulas.semana1;
+
+/**
+ *
+ * @author João Metambo
+ * @author José Seie
+ * @author Alfredo Sebastião
+ * @version 1.0
+ *  
+ * Classe Controladora das classes: Gstor, Caixa e Fachineiro.
+ */
+public class FuncionarioController {
+    
+    private Gestor gestor;
+    
+    public FuncionarioController(Gestor gestor) {
+        this.gestor = gestor;
+    }
+    
+    
+    
+    
+}
