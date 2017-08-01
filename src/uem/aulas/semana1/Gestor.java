f4611aa3c118376dba7d4d1d019675aa3754ad58
Jose Seie <joseeugenioseie@hotmail.com>
2017-08-01 10:23:56 +0200

    Iniciada a documentação.
    Uso dos padrões correctos

---
+++ src/uem/aulas/semana1/Gestor.java
@@ -0,0 +1,15 @@
+
+package uem.aulas.semana1;
+
+/**
+ *
+ * @author João Metambo
+ * @author José Seie
+ * @author Alfredo Sebastião
+ * @version 1.0
+ *  
+ * Classe Modelo do gestor.
+ */
+public class Gestor {
+    
+}
